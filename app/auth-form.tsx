d5e2922700b20a56badbb4397a33521fb4d60a25
LaurelinePBD <laurelinepbd@yahoo.fr>
2023-11-07 17:42:54 +0100

    correct

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -15,7 +15,7 @@ export default function AuthForm() {
       theme="dark"
       showLinks={false}
       providers={[]}
-      redirectTo="http://localhost:3000/auth/callback"
+      redirectTo="https://agence47-1s4o.vercel.app/"
     />
   )
 }

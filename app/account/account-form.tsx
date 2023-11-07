2a815db4269a17b720ffd07b85c53bf7c3044ff2
LaurelinePBD <laurelinepbd@yahoo.fr>
2023-11-07 17:21:42 +0100

    correct

--- app/account/account-form.tsx
+++ app/account/account-form.tsx
@@ -79,59 +79,60 @@ export default function AccountForm({ session }: { session: Session | null }) {
   }
 
   return (
-    <div className="form-widget">
+    <div className="form-widget animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
       <div className='mt-12 mb-8'>
-        <label htmlFor="email" className="mr-8">Email</label>
-        <input id="email" type="text" value={session?.user.email} disabled />
+        <label htmlFor="email" className="text-md">Email</label>
+        <input id="email" type="text" 
+        className="rounded-md px-4 py-2 bg-inherit border mb-6"
+        value={session?.user.email} disabled />
       </div>
       <div>
-        <label htmlFor="NameCollectivity">Nom de votre collectivité</label>
+        <label htmlFor="NameCollectivity" className="text-md">Nom de votre collectivité</label>
         <input
           id="NameCollectivity"
           type="text"
           value={nameCollectivity || ''}
           onChange={(e) => setNameCollectivity(e.target.value)}
-          className="text-black mb-4 ml-12 px-4 py-2"
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
         />
       </div>
 
       <div>
-        <label htmlFor="NameEcole">Nom de l'école de votre collectivité</label>
+        <label htmlFor="NameEcole" className="text-md">Nom de l'école de votre collectivité</label>
         <input
           id="NameEcole"
           type="text"
           value={nameEcole || ''}
           onChange={(e) => setNameEcole(e.target.value)}
-          className="text-black mb-4 ml-12 px-4 py-2"
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
         />
       </div>
 
       <div>
-        <label htmlFor="LastRenovationYear">Année de la dernière rénovation de l'école</label>
+        <label htmlFor="LastRenovationYear" className="text-md">Année de la dernière rénovation de l'école</label>
         <input
           id="LastRenovationYear"
           type="text"
           value={lastRenovationYear || ''}
           onChange={(e) => setLastRenovationYear(e.target.value)}
-          className="text-black mb-4 ml-12 px-4 py-2"
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
         />
       </div>
 
       <div>
-        <label htmlFor="PriceLastRenovation">Prix de la dernière rénovation</label>
+        <label htmlFor="PriceLastRenovation" className="text-md">Prix de la dernière rénovation</label>
         <input
           id="PriceLastRenovation"
           type="text"
           value={priceLastRenovation || ''}
           onChange={(e) => setPriceLastRenovation(e.target.value)}
-          className="text-black mb-4 ml-12 px-4 py-2"
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
         />
       </div>
 
       <div>
         <button
-          className="button primary block font-bold mb-12 border border-white px-2 py-2
-          bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          className="button primary block font-bold mb-12 border border-white px-2 py-2 "
           onClick={() => updateProfile({ nameCollectivity, nameEcole, lastRenovationYear, priceLastRenovation })}
           disabled={loading}
         >
@@ -141,7 +142,7 @@ export default function AccountForm({ session }: { session: Session | null }) {
 
       <div>
         <form action="/auth/signout" method="post">
-          <button className="button block" type="submit">
+          <button className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2" type="submit">
             Deconnexion
           </button>
         </form>
